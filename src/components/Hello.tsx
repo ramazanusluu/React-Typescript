cde5ee85a9ec6dc7252f2cd2395ef4ecc852c9b7
Ramazan USLU <ramazan.uslu@path.com.tr>
2022-06-25 00:35:40 +0300

    React-typescript props kullanimi

---
+++ src/components/Hello.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+// import React, {FC, FunctionComponent} from "react";
+//FC veya FunctionComponent ifadesini burda import ettiğimizde React.FunctionComponent şeklinde yazamamıza gerek kalmaz
+
+interface IProps {
+  name: string;
+  label?: string;
+  description?: string;
+  onSmthHappen: (name: string) => void;
+}
+//React.FunctionComponent yerine FC'de kullanılabilir
+const Hello: React.FunctionComponent<IProps> = ({
+  name,
+  label,
+  description = "Desc",
+  onSmthHappen,
+}) => {
+  return (
+    <>
+      <h1>
+        React Typscript Component - {name} - {description}
+      </h1>
+      {onSmthHappen(name)}
+    </>
+  );
+};
+
+export default Hello;
